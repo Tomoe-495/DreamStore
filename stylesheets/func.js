ac7f886eed036cdc8b060106b7abdfe44aa8049f
Tomoe-495 <hasnainsiddique495@gmail.com>
2022-09-01 19:56:15 +0500

    adjustments

--- stylesheets/func.js
+++ stylesheets/func.js
@@ -17,36 +17,36 @@ let allSects = [home, about, product, contact];
 homebtn.onclick = () => {
     home.style.display = "block";
     for(let i of [about, product, contact]){
-        i.style.display = "none"
+        i.style.display = "none";
     }
     for(let n of navitem){
         n.classList.remove('active');
-    }navitem[0].classList.add('active')
+    }navitem[0].classList.add('active');
 }
 aboutbtn.onclick = () => {
     about.style.display = "block";
     for(let i of [home, product, contact]){
-        i.style.display = "none"
+        i.style.display = "none";
     }
     for(let n of navitem){
         n.classList.remove('active');
-    }navitem[1].classList.add('active')
+    }navitem[1].classList.add('active');
 }
 productbtn.onclick = () => {
     product.style.display = "block";
     for(let i of [about, home, contact]){
-        i.style.display = "none"
+        i.style.display = "none";
     }
     for(let n of navitem){
         n.classList.remove('active');
-    }navitem[2].classList.add('active')
+    }navitem[2].classList.add('active');
 }
 contactbtn.onclick = () => {
     contact.style.display = "block";
     for(let i of [about, product, home]){
-        i.style.display = "none"
+        i.style.display = "none";
     }
     for(let n of navitem){
         n.classList.remove('active');
-    }navitem[4].classList.add('active')
+    }navitem[4].classList.add('active');
 }
