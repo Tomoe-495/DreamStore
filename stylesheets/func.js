d8481341fbba4013ab6e0a336232b5b4d04bc881
Tomoe-495 <hasnainsiddique495@gmail.com>
2022-09-01 19:42:45 +0500

    progress

--- stylesheets/func.js
+++ stylesheets/func.js
@@ -4,6 +4,8 @@ let productbtn = document.getElementsByClassName('product-btn')[0];
 let themebtn = document.getElementsByClassName('theme-btn')[0];
 let contactbtn = document.getElementsByClassName('contact-btn')[0];
 
+let navitem = document.getElementsByClassName('nav-item');
+
 let home = document.getElementsByClassName('home')[0];
 let about = document.getElementsByClassName('about')[0];
 let product = document.getElementsByClassName('products')[0];
@@ -12,15 +14,39 @@ let contact = document.getElementsByClassName('contacts')[0];
 let allSectbtn = [homebtn, aboutbtn, productbtn, contactbtn];
 let allSects = [home, about, product, contact];
 
-function sectAppear(sect){
-    console.log(sect);
-    sect.style.display = "block";
-}
-
-for(let a of allSectbtn){
-    a.onclick = () =>{
-        for(let s of allSects){
-            s.style.display = "none";
-        }
+homebtn.onclick = () => {
+    home.style.display = "block";
+    for(let i of [about, product, contact]){
+        i.style.display = "none"
     }
+    for(let n of navitem){
+        n.classList.remove('active');
+    }navitem[0].classList.add('active')
+}
+aboutbtn.onclick = () => {
+    about.style.display = "block";
+    for(let i of [home, product, contact]){
+        i.style.display = "none"
+    }
+    for(let n of navitem){
+        n.classList.remove('active');
+    }navitem[1].classList.add('active')
+}
+productbtn.onclick = () => {
+    product.style.display = "block";
+    for(let i of [about, home, contact]){
+        i.style.display = "none"
+    }
+    for(let n of navitem){
+        n.classList.remove('active');
+    }navitem[2].classList.add('active')
+}
+contactbtn.onclick = () => {
+    contact.style.display = "block";
+    for(let i of [about, product, home]){
+        i.style.display = "none"
+    }
+    for(let n of navitem){
+        n.classList.remove('active');
+    }navitem[4].classList.add('active')
 }
